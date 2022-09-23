7f7d4a20d46fa5bc41f4925620f36c7ace9320ae
Masha <iiureva.mariia@gmail.com>
2022-09-23 13:59:52 -0700

    adding c

--- function_c.py
+++ function_c.py
@@ -3,7 +3,11 @@ def merge_lists(list_a, list_b):
         a combination of list_a and list_b
         without any duplicate elements.
     """
-    pass
+    #```python
+    return list(set(list_a) | set(list_b))
+
+    #adding comment to check
+
 
 
 if __name__ == "__main__":
